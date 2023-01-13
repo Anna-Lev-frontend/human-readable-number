7b1f2da60ab2c9dd44204c4d48ab77748e4baedf
Anna Levkovich <7604442@gmail.com>
2023-01-14 00:36:02 +0300

    fix: to fix mistakes

--- src/index.js
+++ src/index.js
@@ -58,8 +58,27 @@ module.exports = function toReadable(number) {
         if(numStr[1] === '0' && numStr[2] === '1') {
             return `${arr[numStr[0]]} ${hundred} ${arr[numStr[2]]}`
         }
-       
-
+        if (numStr[1] === '0' && numStr[2] === '6') {
+            return `${arr[numStr[0]]} ${hundred} ${arr[numStr[2]]}`
+        }
+        if (numStr[1] === '0' && numStr[2] === '7') {
+            return `${arr[numStr[0]]} ${hundred} ${arr[numStr[2]]}`
+        }
+        if (numStr[1] === '1' && numStr[2] === '3') {
+            return `${arr[numStr[0]]} ${hundred} ${arr[numStr[1]+'3']}`
+        }
+        if (numStr[1] === '1' && numStr[2] === '9') {
+            return `${arr[numStr[0]]} ${hundred} ${arr[numStr[1] + '9']}`
+        }
+        if (numStr[1] === '0' && numStr[2] === '9') {
+            return `${arr[numStr[0]]} ${hundred} ${arr[numStr[2]]}`
+        }
+        if (numStr[1] === '1' && numStr[2] === '6') {
+            return `${arr[numStr[0]]} ${hundred} ${arr[numStr[1]+'6']}`
+        }
+        if (numStr[1] === '0' && numStr[2] === '2') {
+            return `${arr[numStr[0]]} ${hundred} ${arr[numStr[2]]}`
+        }
         if (numStr[2] === '0') {
             return `${arr[numStr[0]]} ${hundred} ${arr[numStr[1] + '0']}`;
         }
